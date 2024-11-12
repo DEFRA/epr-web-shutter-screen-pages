fffca0f2800c62647c361c094cfcefec8bf734d0
Rushdy Najath <rushdy.najath.external@eviden.com>
2024-11-12 16:11:25 +0000

    Update resetApplicationGatewayCustomeError.ps1

--- pipelines/powershell/resetApplicationGatewayCustomeError.ps1
+++ pipelines/powershell/resetApplicationGatewayCustomeError.ps1
@@ -5,7 +5,7 @@ param (
 )
 
 #Get-AzContext
-#Set-AzContext $gatewayServiceConnection
+Set-AzContext $gatewayServiceConnection
 #'5ac20727-8367-4439-81c1-1ba4c030ecb0'
 # $gatewayServiceConnection
 get-azapplicationGatewayCustomError -ApplicationGateway $appgw
