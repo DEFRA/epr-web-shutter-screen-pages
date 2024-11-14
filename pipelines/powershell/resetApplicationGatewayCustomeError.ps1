d6ad05867238a36462b1f0bd13683d3c8a498f2f
Rushdy Najath <rushdy.najath.external@eviden.com>
2024-11-14 12:39:37 +0000

    Update resetApplicationGatewayCustomeError.ps1

--- pipelines/powershell/resetApplicationGatewayCustomeError.ps1
+++ pipelines/powershell/resetApplicationGatewayCustomeError.ps1
@@ -1,11 +1,11 @@
-[CmdletBinding()]
-param (
-    [Parameter(Mandatory = $true)] [string] $gatewayServiceConnection,
-    [Parameter(Mandatory =$true)] [string] $applicationGatewayRg
-)
+# [CmdletBinding()]
+# param (
+#     [Parameter(Mandatory = $true)] [string] $gatewayServiceConnection,
+#     [Parameter(Mandatory =$true)] [string] $applicationGatewayRg
+# )
 
-Write-Host "gateway Service  Connection $($gatewayServiceConnection)" 
-Write-Host "application Gateway Rg $($applicationGatewayRg)" 
+# Write-Host "gateway Service  Connection $($gatewayServiceConnection)" 
+# Write-Host "application Gateway Rg $($applicationGatewayRg)" 
 
 #Get-AzContext
 #Set-AzContext "5ac20727-8367-4439-81c1-1ba4c030ecb0" #$gatewayServiceConnection
@@ -13,4 +13,5 @@ Write-Host "application Gateway Rg $($applicationGatewayRg)"
 get-azResourceGroup
 #
 # $gatewayServiceConnection
-get-azapplicationGatewayCustomError -ApplicationGateway "SECRWDDEVAG9401"
+#get-azapplicationGatewayCustomError -ApplicationGateway "SECRWDDEVAG9401"
+Get-AzApplicationGatewayAvailableServerVariableAndHeader -ServerVariable
