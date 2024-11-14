656dc18581547c63f05155ca508f7ee473068aa2
Rushdy Najath <rushdy.najath.external@eviden.com>
2024-11-14 14:25:02 +0000

    Update resetApplicationGatewayCustomeError.ps1

--- pipelines/powershell/resetApplicationGatewayCustomeError.ps1
+++ pipelines/powershell/resetApplicationGatewayCustomeError.ps1
@@ -4,8 +4,8 @@ param (
     [Parameter(Mandatory =$true)] [string] $applicationGatewayRg
 )
 
-# Write-Host "gateway Service  Connection $($gatewayServiceConnection)" 
-# Write-Host "application Gateway Rg $($applicationGatewayRg)" 
+Write-Host "gateway Service  Connection $($gatewayServiceConnection)" 
+Write-Host "application Gateway Rg $($applicationGatewayRg)" 
 
 #Get-AzContext
 #Set-AzContext "5ac20727-8367-4439-81c1-1ba4c030ecb0" #$gatewayServiceConnection
