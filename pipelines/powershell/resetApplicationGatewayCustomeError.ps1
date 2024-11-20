369d65460c2b8bd7acae82c5f0928a27783cd90a
Rushdy Najath <rushdy.najath.external@eviden.com>
2024-11-20 15:03:58 +0000

    Update resetApplicationGatewayCustomeError.ps1

--- pipelines/powershell/resetApplicationGatewayCustomeError.ps1
+++ pipelines/powershell/resetApplicationGatewayCustomeError.ps1
@@ -7,33 +7,9 @@ param (
 Write-Host "gateway Service  Connection $($gatewayServiceConnection)" 
 Write-Host "application Gateway Rg $($applicationGatewayRg)" 
 
-#Get-AzContext
-#Set-AzContext "5ac20727-8367-4439-81c1-1ba4c030ecb0" #$gatewayServiceConnection
-
-#get-azResourceGroup
-#
-# $gatewayServiceConnection
-#get-azapplicationGatewayCustomError -ApplicationGateway "SECRWDDEVAG9401"
-#Get-AzApplicationGatewayAvailableServerVariableAndHeader -ServerVariable
-#Get-AzApplicationGatewayAvailableWafRuleSet
-#$AppGw =Get-AzApplicationGateway -Name "SECADPSNDAG1401" -ResourceGroupName "SECADPSNDRG1401"
 
 $AppGw =Get-AzApplicationGateway -Name $gatewayName -ResourceGroupName  $applicationGatewayRg
-Write-Host "----"
 Write-output $AppGw
-Write-Host "----"
-
-#$AppGw = Get-AzApplicationGateway -Name $applicationGatewayRg -ResourceGroupName $applicationGatewayRg
-#$Settings  = Get-AzApplicationGatewayBackendSetting -Name "Settings01" -ApplicationGateway $AppGw
-
-#Get-AzApplicationGatewayCustomError -ApplicationGateway $AppGw #-StatusCode HttpStatus502
-#Get-AzApplicationGatewayListener -Name "Listener01" -ApplicationGateway $Appgw
-#$Listeners = Get-AzApplicationGatewayListener -ApplicationGateway $AppGw
-
-Write-Host "----"
-#Write-output $Listeners
-Write-Host "----"
 Stop-AzApplicationGateway -ApplicationGateway $AppGw
 Start-AzApplicationGateway -ApplicationGateway $AppGw
-
- #Get-AzApplicationGatewayWebApplicationFirewallConfiguration -ApplicationGateway $AppGW
+ 
