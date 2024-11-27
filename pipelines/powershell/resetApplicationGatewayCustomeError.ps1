9617f0fee0f004252a8adac71a2274d138e29b90
Rushdy Najath <rushdy.najath.external@eviden.com>
2024-11-27 16:29:02 +0000

    Update resetApplicationGatewayCustomeError.ps1

--- pipelines/powershell/resetApplicationGatewayCustomeError.ps1
+++ pipelines/powershell/resetApplicationGatewayCustomeError.ps1
@@ -14,6 +14,8 @@ Write-output $AppGw
 
 $SettingsList  = Get-AzApplicationGatewayBackendHttpSetting -ApplicationGateway $AppGw
 
+$AppGws = Set-AzApplicationGatewayBackendHttpSetting -ApplicationGateway $AppGw -Name "Setting02" -Port 88 -Protocol "Https" -CookieBasedAffinity "Disabled" -RequestTimeout 40
+
 #Set-AzApplicationGatewayBackendHttpSetting -ApplicationGateway $AppGw -Name  "obligation-checker-settings" -Port 443  -Protocol "Https" -CookieBasedAffinity Disabled -RequestTimeout 40
 
 Write-output $SettingsList
