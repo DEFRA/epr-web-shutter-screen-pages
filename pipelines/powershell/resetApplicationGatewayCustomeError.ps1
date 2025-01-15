22f33935140ca96fd679b8382d27ae08212dc7a4
Rushdy Najath <rushdy.najath.external@eviden.com>
2025-01-15 13:39:47 +0000

    Update resetApplicationGatewayCustomeError.ps1

--- pipelines/powershell/resetApplicationGatewayCustomeError.ps1
+++ pipelines/powershell/resetApplicationGatewayCustomeError.ps1
@@ -9,19 +9,11 @@ Write-Host "application Gateway Rg $($applicationGatewayRg)"
 
 
 $AppGw =Get-AzApplicationGateway -Name $gatewayName -ResourceGroupName  $applicationGatewayRg
-# $AppGw =Get-AzApplicationGateway
+ $AppGw =Get-AzApplicationGateway
 Write-output $AppGw
 
-$SettingsList  = Get-AzApplicationGatewayBackendHttpSetting -ApplicationGateway $AppGw
-
-$Settingadd = New-AzApplicationGatewayBackendHttpSetting -Name "Setting01" -Port 80 -Protocol Http -CookieBasedAffinity Disabled
-
-Remove-AzApplicationGatewayBackendHttpSetting -ApplicationGateway $AppGw -Name "Setting01"
-Set-AzApplicationGateway -ApplicationGateway $AppGW
-
-#Set-AzApplicationGatewayBackendHttpSetting -ApplicationGateway $AppGw -Name  "obligation-checker-settings" -Port 443  -Protocol "Https" -CookieBasedAffinity Disabled -RequestTimeout 40
-
 Write-output $SettingsList
- ##Stop-AzApplicationGateway -ApplicationGateway $AppGw
- ##Start-AzApplicationGateway -ApplicationGateway $AppGw
+
+ Stop-AzApplicationGateway -ApplicationGateway $AppGw
+ Start-AzApplicationGateway -ApplicationGateway $AppGw
  
