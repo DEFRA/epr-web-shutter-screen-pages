b5ef58e3245cce38cef1a28678ebc32853a6ca76
rnajath <rushdy.najath.external@eviden.com>
2025-01-29 13:51:07 +0000

    44392 update shutter screen (#4)
    * call powershall script
    
    * clean up
    
    * CLEAN UP
    
    * Update azure-pipelines.yml
    
    * Update azure-pipelines.yml
    
    * Update azure-pipelines.yml
    
    * Update azure-pipelines.yml
    
    * Update resetApplicationGatewayCustomeError.ps1
    
    * Update resetApplicationGatewayCustomeError.ps1
    
    * Update resetApplicationGatewayCustomeError.ps1
    
    * update powershell to 5
    
    * Update resetApplicationGatewayCustomeError.ps1
    
    * Update azure-pipelines.yml
    
    * Update azure-pipelines.yml
    
    * Update azure-pipelines.yml
    
    * Update azure-pipelines.yml
    
    * Update resetApplicationGatewayCustomeError.ps1
    
    * Update resetApplicationGatewayCustomeError.ps1
    
    * Update resetApplicationGatewayCustomeError.ps1
    
    * Update resetApplicationGatewayCustomeError.ps1
    
    * TEST
    
    * C
    
    * Update resetApplicationGatewayCustomeError.ps1
    
    * Update resetApplicationGatewayCustomeError.ps1
    
    * Update resetApplicationGatewayCustomeError.ps1
    
    * Update resetApplicationGatewayCustomeError.ps1
    
    * Update resetApplicationGatewayCustomeError.ps1
    
    * Update resetApplicationGatewayCustomeError.ps1
    
    * Update resetApplicationGatewayCustomeError.ps1
    
    * Update resetApplicationGatewayCustomeError.ps1
    
    * Update resetApplicationGatewayCustomeError.ps1
    
    * Update resetApplicationGatewayCustomeError.ps1
    
    * Update resetApplicationGatewayCustomeError.ps1
    
    * Update resetApplicationGatewayCustomeError.ps1
    
    * Update resetApplicationGatewayCustomeError.ps1
    
    * Update resetApplicationGatewayCustomeError.ps1
    
    * Update resetApplicationGatewayCustomeError.ps1
    
    * Update resetApplicationGatewayCustomeError.ps1
    
    * Update resetApplicationGatewayCustomeError.ps1
    
    * Update resetApplicationGatewayCustomeError.ps1
    
    * Update resetApplicationGatewayCustomeError.ps1
    
    * Update resetApplicationGatewayCustomeError.ps1
    
    * Update resetApplicationGatewayCustomeError.ps1
    
    * Update resetApplicationGatewayCustomeError.ps1
    
    * Update resetApplicationGatewayCustomeError.ps1
    
    * Update azure-pipelines.yml
    
    * Update resetApplicationGatewayCustomeError.ps1
    
    * clean up
    
    * add condition only to development
    
    * Update azure-pipelines.yml
    
    * Update azure-pipelines.yml
    
    * Update azure-pipelines.yml
    
    * clean up the variable
    
    * Update resetApplicationGatewayCustomeError.ps1
    
    * update
    
    * Update resetApplicationGatewayCustomeError.ps1
    
    * clean up
    
    * Update development.yaml
    
    * Update Shutter-Page.html
    
    * Update Shutter-Page.html
    
    * Update resetApplicationGatewayCustomeError.ps1
    
    * Update resetApplicationGatewayCustomeError.ps1
    
    * Update resetApplicationGatewayCustomeError.ps1
    
    * Update resetApplicationGatewayCustomeError.ps1
    
    * Update resetApplicationGatewayCustomeError.ps1
    
    * Update resetApplicationGatewayCustomeError.ps1
    
    * Update resetApplicationGatewayCustomeError.ps1
    
    * Update resetApplicationGatewayCustomeError.ps1
    
    * Update resetApplicationGatewayCustomeError.ps1
    
    * Update resetApplicationGatewayCustomeError.ps1
    
    * clean up
    
    * Update resetApplicationGatewayCustomeError.ps1
    
    * Update Shutter-Page.html
    
    * Update resetApplicationGatewayCustomeError.ps1
    
    * run the reset_Application_Gateway on completion of copying_ShutterPage job
    
    * clean up

---
+++ pipelines/powershell/resetApplicationGatewayCustomeError.ps1
@@ -0,0 +1,15 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $true)] [string] $gatewayName,
+    [Parameter(Mandatory =$true)] [string] $applicationGatewayRg
+)
+
+Write-Host "gateway Service  Connection $($gatewayServiceConnection)" 
+Write-Host "application Gateway Rg $($applicationGatewayRg)" 
+
+$AppGw =Get-AzApplicationGateway -Name $gatewayName -ResourceGroupName  $applicationGatewayRg
+Write-output $AppGw
+
+Stop-AzApplicationGateway -ApplicationGateway $AppGw
+Start-AzApplicationGateway -ApplicationGateway $AppGw
+ 
